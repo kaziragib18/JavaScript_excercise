2f7213cfd4b0871b3dc3225b13dfa416235f8735
Kazi Md. Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-06-27 15:14:08 +0600

    Update Exercise.js

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,17 @@
+/**
+ * Kilometer to Meter Unit Conversion
+ * @param Number km
+ * @return Number meter
+ */
+function kilometerToMeter(km) {
+    if (typeof km != "number") {
+        return "Please Input Number Please! ";
+    }
+    if (km < 0) {
+        return "Please Input Positive Unit";
+    }
+    // 1km = 1000 meter
+    return km * 1000;
+}
+
 

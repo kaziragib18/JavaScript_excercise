e8d1222c798ff5bc5857f0210fb7bc20d828f0bc
Kazi Md. Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-06-27 15:15:14 +0600

    Update Exercise.js

--- Exercise.js
+++ Exercise.js
@@ -14,4 +14,30 @@ function kilometerToMeter(km) {
     return km * 1000;
 }
 
+/**
+ * Budget Calculate for electronics store
+ * @param Number clockQuantity , Number phoneQuantity , Number laptopQuantity ,
+ * @return Number totalCost
+ */
+function budgetCalculator(clockQuantity, phoneQuantity, laptopQuantity) {
+    // Validation
+    if (
+        typeof clockQuantity != "number" ||
+        typeof phoneQuantity != "number" ||
+        typeof laptopQuantity != "number"
+    ) {
+        return "Wrong Input! Please Provide all credentials properly";
+    }
+    if (clockQuantity <= 0 || phoneQuantity <= 0 || laptopQuantity <= 0) {
+        return "Wrong Input! Please Provide valid quantity (minimum 1 quantity)";
+    }
+
+    var clockPrice  = clockQuantity * 50; // 1 clock = 50TK
+    var phonePrice  = phoneQuantity * 100; // 1 phone = 100TK
+    var laptopPrice = laptopQuantity * 500; // 1 laptop = 500TK
+
+    var totalCost   = clockPrice + phonePrice + laptopPrice;
+    return totalCost;
+}
+
 
